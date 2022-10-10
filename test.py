358b10d47cb1f827b17702a056b5b1753acb275c
tmor8832 <foamyur@icloud.com>
2022-10-10 13:52:34 +0100

    first push

--- test.py
+++ test.py
@@ -2,22 +2,23 @@ import requests
 
 BASE = "http://0.0.0.0:5000/" #default URL given when you run the flask file main.py
 
-data = [{"likes":1500, "name": "Video1", "views":1000}, 
-        {"likes":2500, "name": "Video2", "views":2000},
-        {"likes":3500, "name": "Video3", "views":3000},
-        {"likes":4500, "name": "Video4", "views":4000},
-        {"likes":5500, "name": "Video5", "views":5000},
-        {"likes":6500, "name": "Video6", "views":20000}]
+data = [{"date":"010719971200", "event": "Motion", "event_info":"Person", "event_link":""}, 
+        {"date":"020719971300", "event": "Motion", "event_info":"Person", "event_link":"https://link.com"}]
 
 for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+    response = requests.put(BASE + "event/", data[i])
+    print(response)
+
 
 input()
-response = requests.get(BASE + "video/2")
+x = "5f9ef148-aa43-4629-82ae-dc2162cf703a"
+response = requests.get(BASE + "event/" + str(x))
 print(response.json())
 input()
-response = requests.delete(BASE + "video/2")
-input()
-response = requests.get(BASE + "video/2")
+response = requests.delete(BASE + "event/" + str(x))
 print(response.json())
+input()
+response = requests.get(BASE + "events/")
+print(response.json())
+input()
+response = requests.put(BASE + "delevents/")
